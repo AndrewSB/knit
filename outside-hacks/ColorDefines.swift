bf24fa146086b9bc8cab5351702f488964278655
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-11 20:28:26 -0700

    add fonts and define colors

--- outside-hacks/ColorDefines.swift
+++ outside-hacks/ColorDefines.swift
@@ -9,7 +9,32 @@
 import Foundation
 
 class Color {
-    enum Stage: String {
-        
+    enum Global {
+        enum Green: String {
+            case Light = "81C784"
+            case Dark = "4CAF50"
+        }
+    }
+    
+    enum Stage {
+        enum LandsEnd: String {
+            case Light = "E84C80"
+            case Dark = "5D1E33"
+        }
+        enum Sutro: String {
+            case Light = "CC7CDA"
+            case Dark = "523258"
+        }
+        enum TwinPeaks: String {
+            case Light = "62B2F1"
+            case Dark = "274761"
+        }
+        enum Panhandle: String {
+            case Light = "FFB04D"
+            case Dark = "67471F"
+        }
+        enum Elsewhere: String {
+            case Light = "A4A4A4"
+        }
     }
 }
